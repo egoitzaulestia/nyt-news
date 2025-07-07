eda62d9b7aaacb4ded0f8afda75a903adaf3496b
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-07 17:16:16 +0200

    npm install with inital dependencies ready

---
+++ src/components/NewsForm/NewsForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const NewsForm = () => {
+  const [data, setData] = useData({});
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="headline"
+        value={data.headline}
+        onChange={handleInputChange}
+        name="headline"
+      />
+      <input
+        type="text"
+        placeholder="headline"
+        value={data.headline}
+        onChange={handleInputChange}
+        name="headline"
+      />
+
+      <textarea
+        placeholder="Write your story here…"
+        name="body"
+        value={data.body}
+        onChange={handleInputChange}
+        rows={6} /* height */
+        cols={40} /* width */
+        style={{ resize: "vertical" }} /* allow only vertical resize */
+      />
+    </form>
+  );
+};
+
+export default NewsForm;
