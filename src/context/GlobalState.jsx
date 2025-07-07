78b083f3f6a138ad707f79032153068da8e4c998
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-07 22:12:30 +0200

    AppReducer converted to newsReducer

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,15 +1,15 @@
 import React, { createContext, useReducer } from "react";
 import axios from "axios";
-import AppReducer from "./AppReducer";
+import newsReducer from "./newsReducer";
 
 const initialState = {
-  character: [],
+  articles: [],
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(newsReducer, initialState);
 
   const getNews = async () => {
     const response = await axios.get();
