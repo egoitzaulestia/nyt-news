a960e2a117f2d5c2105bcca24ef8ae75e91b5eda
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-08 23:50:00 +0200

    Page components imported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Form from "./pages/Form";
+import List from "./pages/List";
 
 function App() {
   return (
     <>
       <Home />
+      <Form />
+      <List />
     </>
   );
 }
