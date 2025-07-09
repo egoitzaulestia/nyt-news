a5e085ff4920991a44e24cd810676f2347432b21
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-09 13:53:09 +0200

    NewsForm component import and mountend in From page component

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,5 +1,13 @@
+import React from "react";
+import NewsForm from "../components/NewsForm/NewsForm";
+
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <main className="page page--form">
+      <h1>Add a News Article</h1>
+      <NewsForm />
+    </main>
+  );
 };
 
 export default Form;
