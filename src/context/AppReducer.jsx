aa73404b34e30bc43e340e428e7fc1d0f22c8898
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-09 13:21:18 +0200

    AppReducer ADD_ARTICLE updated

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,18 +1,59 @@
+// const AppReducer = (state, action) => {
+//   switch (action.type) {
+//     case "SET_ARTICLES":
+//       return {
+//         ...state,
+//         articles: action.payload,
+//         loading: false,
+//         error: null,
+//       };
+//     case "ADD_ARTICLE":
+//       return {
+//         ...state,
+//         article: [...state.articles, action.payload],
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export default AppReducer;
+
 const AppReducer = (state, action) => {
   switch (action.type) {
     case "SET_ARTICLES":
       return {
         ...state,
-        apiArticle: action.payload,
+        apiArticles: action.payload,
+        // We merge saved + API articles
         asticles: [...state.userArticles, ...action.payload],
         loading: false,
         error: null,
       };
 
-    case "ADD_ARTICLE":
+    case "ADD_ARTICLE": {
+      const newUserArticles = [action.payload, ...state.userArticles];
+      localStorage.setItem("userArticles", JSON.stringify(newUserArticles));
       return {
         ...state,
-        article: [...state.articles, action.payload],
+        userArticles: newUserArticles,
+        articles: [...newUserArticles, ...state.apiArticles],
+      };
+    }
+
+    case "FETCH_START":
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case "FETCH_ERROR":
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
       };
 
     default:
